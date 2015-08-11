40db48dec20dc7a30d844aeceb17f39cf3007647
Julien Delasoie <julien.delasoie@epfl.ch>
2015-08-11 17:15:44 +0200

    Improved Patent printing

--- modules/bibformat/lib/elements/bfe_patent.py
+++ modules/bibformat/lib/elements/bfe_patent.py
@@ -1,27 +1,28 @@
 # -*- coding: utf-8 -*-
 """BibFormat element - Prints Patent Number"""
 
-def format(bfo, short="no"):
+def format(bfo, short="no", add_link_to_epo=False):
     """
     Print Patent information.
 
     """
-    # don't show anything at the moment on html brief
     # it may a good idea to put an information like "published or granted" like :
     # if b1:
     #  return "Granted/Published patent"
     # if a1:
     #  return "Pending patent"
     if short != "no":
+        patent_to_print = ""
         patents = bfo.fields('013')
         if patents:
-            # TODO: order by date
             # return only the latest
             for patent in patents:
                 if patent.has_key('a'):
-                    return patent['a']
+                    patent_to_print = patent['a']
+                    return patent_to_print
         return
-    
+
+    url_to_espacenet = "http://worldwide.espacenet.com/searchResults?compact=false&PN=%s&ST=advanced&locale=en_EP&DB=EPODOC"
     patents = bfo.fields('013')
 
     if bfo.lang != 'en':
@@ -48,7 +49,11 @@ def format(bfo, short="no"):
                 # add type of number
                 if patent.has_key('c'):
                     patent_output += ' (' + patent['c'] + ')'
-                    
+
+                if add_link_to_epo:
+                    url_fullfiled = url_to_espacenet % patent['a']
+                    patent_output = '<a href="%s" target="_blank">%s</a>' % (url_fullfiled, patent_output)
+
                 patents_nr.append(patent_output)
                 
                 # priority_dates are not shown, but are the same year as the publication date
