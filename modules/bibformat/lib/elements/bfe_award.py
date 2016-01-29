a98a6c33e749d5902ac28c69f2da5f04f3a60064
Julien Delasoie <julien.delasoie@epfl.ch>
2016-01-29 13:18:02 +0100

    Allow to print multiple awards

--- modules/bibformat/lib/elements/bfe_award.py
+++ modules/bibformat/lib/elements/bfe_award.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """BibFormat element - Prints Award information"""
 
-
 from invenio.bibknowledge import get_kb_mapping
 
 def format(bfo):
@@ -9,21 +8,30 @@ def format(bfo):
     Print Award.
 
     """
-    output = ''
-    award_field = bfo.field('586__a')
-    if award_field and award_field.strip():
-        if not ',' in award_field:
-            award_name = award_field.strip()
+    total_output = []
+
+    for award_field in bfo.fields('586__a'):
+        if award_field and award_field.strip():
+            if not ',' in award_field:
+                award_name = award_field.strip()
+                award_name = get_kb_mapping(kb_name='awards', key=award_name, default=award_name)
+                if type(award_name) == dict:
+                    award_name = award_name['value']
+                total_output.append(award_name)
+
+            award_name = ','.join(award_field.split(',')[:-1])
+            award_year = award_field.split(',')[-1].strip()
             award_name = get_kb_mapping(kb_name='awards', key=award_name, default=award_name)
             if type(award_name) == dict:
                 award_name = award_name['value']
-            return award_name
+            total_output.append('%s, %s' % (award_name, award_year))
 
-        award_name = ','.join(award_field.split(',')[:-1])
-        award_year = award_field.split(',')[-1].strip()
-        award_name = get_kb_mapping(kb_name='awards', key=award_name, default=award_name)
-        if type(award_name) == dict:
-            award_name = award_name['value']
-        output = '%s, %s' % (award_name, award_year)
                
-    return output
+    return "<br>".join(total_output)
+
+def escape_values(bfo):
+    """
+    Called by BibFormat in order to check if output of this element
+    should be escaped.
+    """
+    return 0
