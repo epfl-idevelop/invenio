f9d35d3c8e0a8cd6afa5632db9ec06123f754310
Julien Delasoie <julien.delasoie@epfl.ch>
2016-07-19 12:42:43 +0200

    Dont throw an error when the 035 is not well formatted

--- modules/bibformat/lib/elements/bfe_references.py
+++ modules/bibformat/lib/elements/bfe_references.py
@@ -37,27 +37,29 @@ def format(bfo):
         if match:
             extra_id = match.group('id')
             cataloger = match.group('cataloger')
-        if cataloger == 'ISI':
-            url = 'http://ws.isiknowledge.com/cps/openurl/service?url_ver=Z39.88-2004&amp;rft_id=info:ut/%s' % extra_id
-            output.append('<a href="%s">%s</a>' % (url, _("View record in Web of Science")))
-        elif cataloger == 'Scopus':
-            url = 'http://www.scopus.com/scopus/openurl/link.url?ctx_ver=Z39.88-2004&amp;rfr_id=http://infoscience.epfl.ch&amp;rft_id=info:eid/%s' % extra_id
-            output.append('<a href="%s">%s</a>' % (url, _("View record in Scopus")))
-        elif cataloger == 'PMID':
-            url = 'http://www.ncbi.nlm.nih.gov/sites/entrez?cmd=Retrieve&db=PubMed&list_uids=%s&dopt=Abstract' % extra_id
-            output.append('<a href="%s">%s</a>' % (url, _("View record in PubMed")))
-        elif cataloger == 'arXiv':
-            url = 'http://www.arxiv.org/openurl-resolver?rft_id=%s&amp;url_ver=Z39.88-2004' % extra_id
-            output.append('<a href="%s">%s</a>' % (url, _("View record in arXiv")))
-        elif cataloger == 'EV':
-            pass
-        elif cataloger == 'SzZuIDS NEBIS':
-            if bfo.lang == 'fr':
-                url = 'http://library.epfl.ch/nebis-redir/?record=%s' % extra_id
-            else:
-                url = 'http://library.epfl.ch/en/nebis-redir/?record=%s' % extra_id
-            
-            output.append('<a href="%s">%s</a>' % (url, _("Print copy in library catalog")))
+
+            if extra_id and cataloger:
+                if cataloger == 'ISI':
+                    url = 'http://ws.isiknowledge.com/cps/openurl/service?url_ver=Z39.88-2004&amp;rft_id=info:ut/%s' % extra_id
+                    output.append('<a href="%s">%s</a>' % (url, _("View record in Web of Science")))
+                elif cataloger == 'Scopus':
+                    url = 'http://www.scopus.com/scopus/openurl/link.url?ctx_ver=Z39.88-2004&amp;rfr_id=http://infoscience.epfl.ch&amp;rft_id=info:eid/%s' % extra_id
+                    output.append('<a href="%s">%s</a>' % (url, _("View record in Scopus")))
+                elif cataloger == 'PMID':
+                    url = 'http://www.ncbi.nlm.nih.gov/sites/entrez?cmd=Retrieve&db=PubMed&list_uids=%s&dopt=Abstract' % extra_id
+                    output.append('<a href="%s">%s</a>' % (url, _("View record in PubMed")))
+                elif cataloger == 'arXiv':
+                    url = 'http://www.arxiv.org/openurl-resolver?rft_id=%s&amp;url_ver=Z39.88-2004' % extra_id
+                    output.append('<a href="%s">%s</a>' % (url, _("View record in arXiv")))
+                elif cataloger == 'EV':
+                    pass
+                elif cataloger == 'SzZuIDS NEBIS':
+                    if bfo.lang == 'fr':
+                        url = 'http://library.epfl.ch/nebis-redir/?record=%s' % extra_id
+                    else:
+                        url = 'http://library.epfl.ch/en/nebis-redir/?record=%s' % extra_id
+
+                    output.append('<a href="%s">%s</a>' % (url, _("Print copy in library catalog")))
         
         
     return ''.join(['<li>%s</li>' % elem for elem in output])
