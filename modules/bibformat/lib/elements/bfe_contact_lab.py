14a27d33dd3f4fb5908bf6ccc73633dc317762e2
Julien Delasoie <julien.delasoie@epfl.ch>
2017-02-27 15:58:00 +0100

    Set special rule for TTO as Contacts lab

--- modules/bibformat/lib/elements/bfe_contact_lab.py
+++ modules/bibformat/lib/elements/bfe_contact_lab.py
@@ -10,10 +10,32 @@ def format(bfo, kb_name, kb_url):
     
     @param kb_name a knowledge base use to translate the lab identifier
     @param kb_url a knowledge base use to translate the lab identifier
-    """ 
-    lab_identifiers = bfo.fields("909C0p", escape=2)
+    """
     out = []
+    lab_identifiers = bfo.fields("909C0p", escape=2)
+
+    # first check if we are in tto mode, as it has his own rules
+    is_tto = False
+
+    if 'TTO' in lab_identifiers:
+        is_tto = True
+    else:
+        # check in source origin
+        external_ids = bfo.fields('035__a')
+
+        for field in external_ids:
+            if 'TTO' in field:
+                is_tto = True
+
+    if bfo.field('980__a') == 'PATENT' and is_tto:
+        out.append('<a href="http://tto.epfl.ch/">Technology Transfer Office</a>')
+
+    # normal step for all laboratories
     for lab in lab_identifiers:
+        if lab == 'TTO':
+            # already done
+            continue
+
         lab_name = bfo.kb(kb_name, lab)
         lab_url = bfo.kb(kb_url, lab)
         if lab_url:
@@ -21,17 +43,6 @@ def format(bfo, kb_name, kb_url):
         else:
             out.append('%s' % lab_name)
 
-    # Add the TTO if we have a patent
-    is_tto = False
-
-    external_ids = bfo.fields('035__a')
-
-    for field in external_ids:
-        if 'TTO' in field:
-            is_tto = True
-
-    if bfo.field('980__a') == 'PATENT' and is_tto:
-        out.append('<a href="http://tto.epfl.ch/">Technology Transfer Office</a>')
 
     return ''.join(['<li>%s</li>' % elem for elem in out])
 
