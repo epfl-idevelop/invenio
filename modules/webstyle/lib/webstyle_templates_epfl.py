2e31a0ba1d63bc7e48a1bfcd55743623a59558e3
William Belle <william.belle@gmail.com>
2017-05-11 13:44:49 +0200

    update GA and add ip anonymization

--- modules/webstyle/lib/webstyle_templates_epfl.py
+++ modules/webstyle/lib/webstyle_templates_epfl.py
@@ -116,21 +116,16 @@ class Template(webstyle_templates.Template):
       </ul>
     </div>
 %(pagefooteradd)s
-  <script type="text/javascript">
-  
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-1830336-6']);
-      _gaq.push(['_setDomainName', 'infoscience.epfl.ch']);
-      
-      _gaq.push(['_trackPageview']);
+  <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-  
-    </script>
+      ga('create', 'UA-1830336-6', 'infoscience.epfl.ch');
+      ga('set', 'anonymizeIp', true);
+      ga('send', 'pageview');
+  </script>
 """ % {
                      'about': ln=='fr' and 'a-propos' or 'about',
                      'about_label': _("About Infoscience"),
